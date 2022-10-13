a9ae50e2c8245b322594a8a9044bc81605821863
Harsh <harshyuvasoft394.com>
2022-10-13 15:44:21 +0530

    commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+   devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
